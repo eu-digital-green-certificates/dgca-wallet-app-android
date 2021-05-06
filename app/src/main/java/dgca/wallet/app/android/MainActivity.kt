1a6cd29b0dacb9c671b39b0000ad3c75cacdb2de
Oleksandr Sarapulov <oleksandr.sarapulov@globallogic.com>
2021-05-06 22:25:15 +0300

    Added biometric authentication and QR code scanning

--- app/src/main/java/dgca/wallet/app/android/MainActivity.kt
+++ app/src/main/java/dgca/wallet/app/android/MainActivity.kt
@@ -2,10 +2,12 @@ package dgca.wallet.app.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
         setContentView(R.layout.activity_main)
     }
 }
