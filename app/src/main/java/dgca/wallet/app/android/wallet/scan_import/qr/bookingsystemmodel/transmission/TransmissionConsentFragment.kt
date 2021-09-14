fdab419882540b76339e18bbf84f741575257b10
Mykhailo Nester <nsmisha.dev@gmail.com>
2021-09-14 20:43:36 +0300

    - add UI for consent transmission;

---
+++ app/src/main/java/dgca/wallet/app/android/wallet/scan_import/qr/bookingsystemmodel/transmission/TransmissionConsentFragment.kt
@@ -0,0 +1,91 @@
+/*
+ *  ---license-start
+ *  eu-digital-green-certificates / dgca-verifier-app-android
+ *  ---
+ *  Copyright (C) 2021 T-Systems International GmbH and all other contributors
+ *  ---
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  ---license-end
+ *
+ *  Created by mykhailo.nester on 14/09/2021, 19:54
+ */
+
+package dgca.wallet.app.android.wallet.scan_import.qr.bookingsystemmodel.transmission
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import dagger.hilt.android.AndroidEntryPoint
+import dgca.wallet.app.android.MainActivity
+import dgca.wallet.app.android.R
+import dgca.wallet.app.android.base.BindingFragment
+import dgca.wallet.app.android.databinding.FragmentTransmissionConsentBinding
+
+@AndroidEntryPoint
+class TransmissionConsentFragment : BindingFragment<FragmentTransmissionConsentBinding>() {
+
+    private val args by navArgs<TransmissionConsentFragmentArgs>()
+    private val viewModel by viewModels<TransmissionConsentViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as MainActivity).clearBackground()
+
+        viewModel.event.observe(this) { event ->
+            event.getContentIfNotHandled()?.let {
+                onViewModelEvent(it)
+            }
+        }
+        viewModel.init()
+    }
+
+    override fun onCreateBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentTransmissionConsentBinding =
+        FragmentTransmissionConsentBinding.inflate(inflater, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.title.text = args.certModel.title
+        binding.description.text = getString(R.string.valid_until, args.certModel.validUntil)
+
+        viewModel.uiEvent.observe(viewLifecycleOwner) { event -> onViewModelUiEvent(event.peekContent()) }
+
+        binding.grantPermission.setOnClickListener {
+            viewModel.onPermissionAccepted()
+        }
+    }
+
+    private fun onViewModelUiEvent(event: TransmissionConsentViewModel.TransmissionConsentUiEvent) {
+        when (event) {
+            TransmissionConsentViewModel.TransmissionConsentUiEvent.OnHideLoading -> binding.progressView.isVisible = false
+            TransmissionConsentViewModel.TransmissionConsentUiEvent.OnShowLoading -> binding.progressView.isVisible = true
+            TransmissionConsentViewModel.TransmissionConsentUiEvent.OnError -> {
+            }
+        }
+    }
+
+    private fun onViewModelEvent(event: TransmissionConsentViewModel.TransmissionConsentEvent) {
+        when (event) {
+            TransmissionConsentViewModel.TransmissionConsentEvent.OnPermissionAccepted -> {
+                Toast.makeText(requireContext(), "Certificate transferred", Toast.LENGTH_SHORT).show()
+                findNavController().popBackStack(R.id.certificatesFragment, false)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/dgca/wallet/app/android/wallet/scan_import/qr/bookingsystemmodel/transmission/TransmissionConsentViewModel.kt
@@ -0,0 +1,75 @@
+/*
+ *  ---license-start
+ *  eu-digital-green-certificates / dgca-verifier-app-android
+ *  ---
+ *  Copyright (C) 2021 T-Systems International GmbH and all other contributors
+ *  ---
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  ---license-end
+ *
+ *  Created by mykhailo.nester on 14/09/2021, 19:54
+ */
+
+package dgca.wallet.app.android.wallet.scan_import.qr.bookingsystemmodel.transmission
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dgca.wallet.app.android.Event
+import dgca.wallet.app.android.data.WalletRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class TransmissionConsentViewModel @Inject constructor(
+    private val walletRepository: WalletRepository
+) : ViewModel() {
+
+    private val _uiEvent = MutableLiveData<Event<TransmissionConsentUiEvent>>()
+    val uiEvent: LiveData<Event<TransmissionConsentUiEvent>> = _uiEvent
+
+    private val _event = MutableLiveData<Event<TransmissionConsentEvent>>()
+    val event: LiveData<Event<TransmissionConsentEvent>> = _event
+
+    fun init() {
+
+    }
+
+    fun onPermissionAccepted() {
+        viewModelScope.launch {
+            _uiEvent.value = Event(TransmissionConsentUiEvent.OnShowLoading)
+
+            withContext(Dispatchers.IO) {
+                // TODO: API Calls
+                delay(1500)
+            }
+            _uiEvent.value = Event(TransmissionConsentUiEvent.OnHideLoading)
+            _event.value = Event(TransmissionConsentEvent.OnPermissionAccepted)
+        }
+    }
+
+    sealed class TransmissionConsentUiEvent {
+        object OnShowLoading : TransmissionConsentUiEvent()
+        object OnHideLoading : TransmissionConsentUiEvent()
+        object OnError : TransmissionConsentUiEvent()
+    }
+
+    sealed class TransmissionConsentEvent {
+        object OnPermissionAccepted : TransmissionConsentEvent()
+    }
+}
