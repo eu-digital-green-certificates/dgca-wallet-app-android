b7c6d6c53b684c2fa79a32581725685559e7c908
Mykhailo Nester <nsmisha.dev@gmail.com>
2021-09-22 15:17:18 +0300

    - add value set identifiers to local DB; (#123)
    * - add value set identifiers to local DB;
    
    * - add worker retry policy;
    
    * - update user agent;

--- app/src/main/java/dgca/wallet/app/android/data/local/rules/EngineDatabase.kt
+++ app/src/main/java/dgca/wallet/app/android/data/local/rules/EngineDatabase.kt
@@ -27,11 +27,19 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import dgca.wallet.app.android.data.local.countries.CountriesDao
 import dgca.wallet.app.android.data.local.countries.CountryLocal
+import dgca.wallet.app.android.data.local.valuesets.ValueSetIdentifierLocal
 import dgca.wallet.app.android.data.local.valuesets.ValueSetLocal
 import dgca.wallet.app.android.data.local.valuesets.ValueSetsDao
 
 @Database(
-    entities = [RuleIdentifierLocal::class, RuleLocal::class, DescriptionLocal::class, CountryLocal::class, ValueSetLocal::class],
+    entities = [
+        RuleIdentifierLocal::class,
+        RuleLocal::class,
+        DescriptionLocal::class,
+        CountryLocal::class,
+        ValueSetLocal::class,
+        ValueSetIdentifierLocal::class
+    ],
     version = 1
 )
 @TypeConverters(Converters::class)
