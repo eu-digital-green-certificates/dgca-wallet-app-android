0a292022b00266e60dba5b7cf2d0b38c0e1ca820
oleksandrsarapulovgl <82441124+oleksandrsarapulovgl@users.noreply.github.com>
2022-03-30 13:24:44 +0300

    Version update 1.3.0(37) -> 1.3.0(40) (#177)

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -28,7 +28,7 @@ object Config {
     const val targetSdk = 29
     val javaVersion = JavaVersion.VERSION_1_8
 
-    const val versionCode = 37
+    const val versionCode = 40
     const val versionName = "1.3.0"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
